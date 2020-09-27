514c9402341bc538fa0bac39f8f736ea4a46b746
kelasov <kelasovg@mail.ru>
2020-09-27 23:09:27 +0300

    init

---
+++ src/Pr5/MovableCircle.java
@@ -0,0 +1,32 @@
+package Pr5;
+
+public class MovableCircle implements Movable{
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "MovableCircle{" +
+                "radius=" + radius +
+                ", center=" + center +
+                '}';
+    }
+
+    @Override
+    public void moveUp() {
+    }
+    @Override
+    public void moveDown() {
+    }
+    @Override
+    public void moveLeft() {
+    }
+    @Override
+    public void moveRight() {
+    }
+}
